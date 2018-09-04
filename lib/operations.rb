5e996980c13d9f700f7487ace9e4b3ce969a3554
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-04 17:33:44 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
   if speed < 40
     true
-    elsif speed > 60
+  elsif speed > 60
     true
   else
     false
@@ -10,5 +10,6 @@ end
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? true : false
+end
 end
